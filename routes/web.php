038b4521209bf88baac863eb6c3bca72f2606423
Rhido <muhammadrhidoakbar@gmail.com>
2026-04-29 19:41:17 +0700

    tambah autentikasi dan proteksi halaman produk

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\ProdukController;
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('produk', ProdukController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+use App\Http\Controllers\ProdukController;
+Route::resource('produk', ProdukController::class)->middleware('auth');
+
+require __DIR__.'/auth.php';
